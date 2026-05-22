d35c6f06a52c240da4af600f4c2e699983a0af5f
Parthiv <g.parthiv119@gmail.com>
2026-05-23 02:04:35 +0530

    Implement emotion detection functionality with Flask integration and unit tests

---
+++ test_emotion_detection.py
@@ -0,0 +1,26 @@
+import unittest
+from EmotionDetection import emotion_detector
+
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_joy(self):
+        response = emotion_detector("I am glad this happened")
+        self.assertEqual(response['dominant_emotion'], 'joy')
+
+    def test_anger(self):
+        response = emotion_detector("I am really mad about this")
+        self.assertEqual(response['dominant_emotion'], 'anger')
+
+    def test_disgust(self):
+        response = emotion_detector("I feel disgusted")
+        self.assertEqual(response['dominant_emotion'], 'disgust')
+
+    def test_sadness(self):
+        response = emotion_detector("I am so sad about this")
+        self.assertEqual(response['dominant_emotion'], 'sadness')
+
+    def test_fear(self):
+        response = emotion_detector("I am afraid of this")
+        self.assertEqual(response['dominant_emotion'], 'fear')
+
+unittest.main()
